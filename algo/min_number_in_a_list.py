c8860ab4dd4af04654a97e3db388887b8ea6d17a
chandan <chandansau100@gmail.com>
2019-12-21 10:32:53 +0530

    max and min number in a list

---
+++ algo/min_number_in_a_list.py
@@ -0,0 +1,26 @@
+# lenear search  complex city=  o(N)
+
+
+def max_number(num):
+    maxi = num[0]
+    # find max from list
+    for nu in num:
+        if nu < maxi:
+            maxi = nu
+    return maxi
+
+
+num = [10, 20, 300, 40, 600]
+print(max_number(num))
+
+
+# --------------------2nd process --------------------
+
+
+def max_number(num):
+    num.sort()
+    return num[0]
+
+
+num = [20, 300, 40, 600, 10]
+print(max_number(num))
