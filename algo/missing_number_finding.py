024d70abf341d95a95f88a490083e84225242263
chandan <chandansau100@gmail.com>
2019-12-24 16:33:58 +0530

    ms number

--- algo/missing_number_finding.py
+++ algo/missing_number_finding.py
@@ -8,3 +8,13 @@ def missing_number(nu):
 
 nu = int(input("Enter any number"))
 missing_number(nu)
+
+
+# ----------------2nd process ----------------------
+
+def missing_number(nu):
+    return str(nu) + " present in b/w 1 to 100" if nu in (_ for _ in range(101)) else 'Not present'
+
+
+nu = int(input("Enter any number"))
+print(missing_number(nu))
